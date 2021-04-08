c12b36d3411df428dea7a1801195a21574e94af1
imcoala <guilherme.snt99@gmail.com>
2021-04-08 03:03:38 -0300

    definindo repositorio de clientes

---
+++ src/repositories/ClientsRepository.ts
@@ -0,0 +1,7 @@
+import { EntityRepository, Repository } from "typeorm";
+import { Client } from "../entities/Client";
+
+@EntityRepository(Client)
+class ClientsRepository extends Repository<Client> {}
+
+export { ClientsRepository }
