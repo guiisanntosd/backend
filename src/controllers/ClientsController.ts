1ed6b1b7f5f7073eabbd6cda20cf1ebdfde36db7
imcoala <guilherme.snt99@gmail.com>
2021-04-08 04:56:08 -0300

    controller e rotas de clientes finalizados

--- src/controllers/ClientsController.ts
+++ src/controllers/ClientsController.ts
@@ -3,6 +3,62 @@ import { getCustomRepository } from "typeorm";
 import { ClientsRepository } from "../repositories/ClientsRepository";
 
 class ClientsController {
+  async index(req: Request, res: Response) {
+    const repository = getCustomRepository(ClientsRepository)
+
+    const clients = await repository.find()
+
+    return res.json(clients)
+  }
+
+  async show(req: Request, res: Response) {
+    const repository = getCustomRepository(ClientsRepository)
+    const { id } = req.params
+
+    const client = await repository.findOneOrFail(id).catch(() => {
+        return res.json({
+          error: 'Cliente não encontrado!'
+        })
+      }
+    )
+
+    return res.status(200).json(client)
+  }
+
+  async update(req: Request, res: Response) {
+    const repository = getCustomRepository(ClientsRepository)
+    const { id } = req.params
+    const { name, cpf, genre, email } = req.body
+
+    const client = await repository.findOne(id)
+    if(!client) {
+      return res.status(400).json({
+        error: 'Cliente não existe!'
+      })
+    }
+    
+    const updatedClient = repository.create({ name, cpf, genre, email })
+    await repository.update(client, updatedClient)
+
+    return res.status(200).json(updatedClient)
+  }
+
+  async delete(req: Request, res: Response) {
+    const repository = getCustomRepository(ClientsRepository)
+    const { id } = req.params
+
+    const client = await repository.findOne(id)
+    if(!client) {
+      return res.status(400).json({
+        error: 'Cliente não existe!'
+      })
+    }
+
+    await repository.delete(client)
+
+    return res.status(200).json(client)
+  }
+
   async create(req: Request, res: Response) {
     const repository = getCustomRepository(ClientsRepository)
     const { name, cpf, genre, email } = req.body
--- src/routes.ts
+++ src/routes.ts
@@ -4,6 +4,14 @@ import ClientsController from './controllers/ClientsController'
 
 const router = Router()
 
+router.get('/clientes', ClientsController.index)
+router.get('/clientes/:id', ClientsController.show)
+
 router.post('/clientes', ClientsController.create)
+
+router.put('/clientes/:id', ClientsController.update)
+
+router.delete('/clientes/:id', ClientsController.delete)
+
 
 export { router }
